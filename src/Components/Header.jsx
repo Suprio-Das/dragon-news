485991aad1a6615f17cff2a71e6b87ba121bdc09
Suprio-Das <priyamd507@gmail.com>
2025-02-10 23:47:44 +0600

    Dynamic Time and Date added using Moment.js package

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -7,6 +8,7 @@ const Header = () => {
                 <img src={logo} alt="Dragon_News_Logo" className='w-[400px]' />
             </div>
             <h2 className='text-[18px] text-[#706F6F]'>Journalism Without Fear or Favour</h2>
+            <p className='text-[20px] text-[#706F6F]'><span className='font-semibold text-black'>{moment().format("dddd")}, </span>{moment().format("MMMM, YYYY")}</p>
         </div>
     );
 };
