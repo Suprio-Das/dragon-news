1f35a0864e9110580036055f77fc3b30bc22f4c0
Suprio-Das <priyamd507@gmail.com>
2025-02-11 00:02:01 +0600

    Latest News Template Design

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,7 +1,8 @@
 const LatestNews = () => {
     return (
-        <div>
+        <div className="flex items-center gap-x-5 bg-[#F3F3F3] p-3">
             <p className="bg-[#D72050] inline text-white px-3 py-2">Latest News</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea perferendis velit quae facilis vero ullam ratione facere! Sequi, veniam quae!</p>
         </div>
     );
 };
