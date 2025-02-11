43e6af36cb6e1a4e463cc327ba8941903326ea6c
Suprio-Das <priyamd507@gmail.com>
2025-02-11 22:05:30 +0600

    User Icon and Login Button added to the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
-
+import userIcon from '../assets/user.png'
 const Navbar = () => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
             <div></div>
             <div className="nav space-x-5">
                 <Link to="/">Home</Link>
                 <Link to="/career">Career</Link>
                 <Link to="/about">About</Link>
             </div>
-            <div></div>
+            <div className="flex items-center space-x-5">
+                <div>
+                    <img src={userIcon} alt="" />
+                </div>
+                <button className="btn bg-[#403F3F] rounded-none px-8 text-white w-[140px] h-[40px]">Login</button>
+            </div>
         </div>
     );
 };
