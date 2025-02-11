b37f6e137c0954a9681f50cabc25b8d0942e25d2
Suprio-Das <priyamd507@gmail.com>
2025-02-12 01:06:15 +0600

    All Categories are shown on the left aside dynamically

--- src/Components/LayoutComponents/LeftSidebar.jsx
+++ src/Components/LayoutComponents/LeftSidebar.jsx
@@ -10,6 +10,14 @@ const LeftSidebar = () => {
     return (
         <div>
             <h2 className="font-semibold text-[#403F3F] text-[20px]">All Category</h2>
+            <h2 className="bg-[#E7E7E7] font-semibold py-3 text-center my-2 rounded-sm">National News</h2>
+            <div className="flex items-center justify-center">
+                <div className="flex flex-col gap-3">
+                    {
+                        categories.map((category) => <button className="text-left text-[#9F9F9F] hover:cursor-pointer hover:underline" key={category.category_id}>{category.category_name}</button>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
