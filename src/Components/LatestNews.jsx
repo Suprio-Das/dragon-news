a668cbfe1ff59c71601f2ef4e18a50260cb9a033
Suprio-Das <priyamd507@gmail.com>
2025-02-11 20:32:29 +0600

    Marquee design using React Marquee

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,8 +1,24 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const LatestNews = () => {
     return (
         <div className="flex items-center gap-x-5 bg-[#F3F3F3] p-3">
-            <p className="bg-[#D72050] inline text-white px-3 py-2">Latest News</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing eliis vero ullam ratione facere! Sequi, veniam quae!</p>
+            <p className="bg-[#D72050] text-white px-4 py-2 text-nowrap">Latest News</p>
+            <Marquee pauseOnHover={true} speed={70} className="w-full">
+                <span className="mr-10 ">
+                    <Link to="/news" className="hover:underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, perspiciatis?</Link>
+                </span>
+                <span className="mr-10">
+                    <Link to="/news" className="hover:underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, perspiciatis?</Link>
+                </span>
+                <span className="mr-10">
+                    <Link to="/news" className="hover:underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, perspiciatis?</Link>
+                </span>
+                <span className="mr-10">
+                    <Link to="/news" className="hover:underline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, perspiciatis?</Link>
+                </span>
+            </Marquee>
         </div>
     );
 };
