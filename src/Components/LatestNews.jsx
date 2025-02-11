f676a0665bcb44b7914af015dccf8e86365d45ef
Suprio-Das <priyamd507@gmail.com>
2025-02-11 20:14:44 +0600

    react-fast-marquee installed and Web title changed

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -2,7 +2,7 @@ const LatestNews = () => {
     return (
         <div className="flex items-center gap-x-5 bg-[#F3F3F3] p-3">
             <p className="bg-[#D72050] inline text-white px-3 py-2">Latest News</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea perferendis velit quae facilis vero ullam ratione facere! Sequi, veniam quae!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing eliis vero ullam ratione facere! Sequi, veniam quae!</p>
         </div>
     );
 };
