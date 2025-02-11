3ab4710e19c136f2d474441adf811955b66d597e
Suprio-Das <priyamd507@gmail.com>
2025-02-12 00:14:38 +0600

    Navbar changed and Implemented Daisyui navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,18 +2,48 @@ import { Link } from "react-router-dom";
 import userIcon from '../assets/user.png'
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center">
-            <div></div>
-            <div className="nav space-x-5">
-                <Link to="/">Home</Link>
-                <Link to="/career">Career</Link>
-                <Link to="/about">About</Link>
+        // <div className="flex justify-between items-center">
+        //     <div></div>
+        //     <div className="nav space-x-5">
+        //         <Link to="/">Home</Link>
+        //         <Link to="/career">Career</Link>
+        //         <Link to="/about">About</Link>
+        //     </div>
+        //     <div className="flex items-center space-x-5">
+        //         <div>
+        //             <img src={userIcon} alt="" />
+        //         </div>
+        //         <button className="btn bg-[#403F3F] rounded-none px-8 text-white">Login</button>
+        //     </div>
+        // </div>
+        <div className="navbar">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/career">Career</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl"></a>
             </div>
-            <div className="flex items-center space-x-5">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/career">Career</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end space-x-2">
                 <div>
                     <img src={userIcon} alt="" />
                 </div>
-                <button className="btn bg-[#403F3F] rounded-none px-8 text-white w-[140px] h-[40px]">Login</button>
+                <button className="btn bg-[#403F3F] rounded-none px-8 text-white">Login</button>
             </div>
         </div>
     );
