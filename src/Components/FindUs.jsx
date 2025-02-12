d2c6c1cc8b0be57d3c9af63df2d1a8c5e902e8a1
Suprio-Das <priyamd507@gmail.com>
2025-02-13 00:36:13 +0600

    Social Icon Designed

--- src/Components/FindUs.jsx
+++ src/Components/FindUs.jsx
@@ -3,10 +3,16 @@ const FindUs = () => {
     return (
         <div className="my-11">
             <h2 className="font-semibold text-[20px] my-1">Find Us On</h2>
-            <div className="join join-vertical w-full *:justify-start *:h-14">
-                <button className="btn join-item"><IoLogoFacebook /> Facebook</button>
-                <button className="btn join-item"><IoLogoTwitter /> Twitter</button>
-                <button className="btn join-item"><IoLogoInstagram /> Instagram</button>
+            <div className="join join-vertical w-full *:justify-start *:h-14 *:bg-white">
+                <button className="btn join-item">
+                    <span className="bg-gray-200 text-lg p-1 rounded-full"><IoLogoFacebook className="text-blue-500" /></span> Facebook
+                </button>
+                <button className="btn join-item">
+                    <span className="bg-gray-200 text-lg p-1 rounded-full"><IoLogoTwitter className="text-blue-500" /></span> Twitter
+                </button>
+                <button className="btn join-item">
+                    <span className="bg-gray-200 text-lg p-1 rounded-full"><IoLogoInstagram className="text-pink-500" /></span> Instagram
+                </button>
             </div>
         </div>
     );
