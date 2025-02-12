3a201038d8b0a3762ccb3b189fb3b904fbada08e
Suprio-Das <priyamd507@gmail.com>
2025-02-12 23:59:28 +0600

    Login Button Added

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -1,7 +1,11 @@
 const SocialLogin = () => {
     return (
         <div>
-            This is social login.
+            <h2 className="font-semibold">Login With</h2>
+            <div>
+                <button className="btn my-1">Login With Google</button>
+                <button className="btn my-1">Login With Github</button>
+            </div>
         </div>
     );
 };
