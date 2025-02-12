7779bc9171e09c58bb6dfa6e6843bbc1a3bcbdd4
Suprio-Das <priyamd507@gmail.com>
2025-02-13 00:04:08 +0600

    Google and Github icon added to the login buttons

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -1,10 +1,12 @@
+import { FaGoogle } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
 const SocialLogin = () => {
     return (
         <div>
-            <h2 className="font-semibold">Login With</h2>
+            <h2 className="font-semibold text-[20px] my-1">Login With</h2>
             <div>
-                <button className="btn my-1">Login With Google</button>
-                <button className="btn my-1">Login With Github</button>
+                <button className="btn my-1 w-[200px]"><FaGoogle /> Login With Google</button>
+                <button className="btn my-1 w-[200px]"><IoLogoGithub /> Login With Github</button>
             </div>
         </div>
     );
