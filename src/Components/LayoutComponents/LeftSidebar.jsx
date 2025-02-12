6f3b3e0cce2143475c8b6c5c6c1137b083679b21
Suprio-Das <priyamd507@gmail.com>
2025-02-13 00:44:18 +0600

    Left Sidebar Category Changed

--- src/Components/LayoutComponents/LeftSidebar.jsx
+++ src/Components/LayoutComponents/LeftSidebar.jsx
@@ -10,8 +10,8 @@ const LeftSidebar = () => {
     return (
         <div>
             <h2 className="font-semibold text-[#403F3F] text-[20px]">All Category</h2>
-            <h2 className="bg-[#E7E7E7] font-semibold py-3 text-center my-2 rounded-sm">National News</h2>
-            <div className="flex items-center justify-center">
+            {/* <h2 className="bg-[#E7E7E7] font-semibold py-3 text-center my-2 rounded-sm">National News</h2> */}
+            <div className="flex items-center justify-center my-5">
                 <div className="flex flex-col gap-3">
                     {
                         categories.map((category) => <button className="text-left text-[#9F9F9F] hover:cursor-pointer hover:underline" key={category.category_id}>{category.category_name}</button>)
