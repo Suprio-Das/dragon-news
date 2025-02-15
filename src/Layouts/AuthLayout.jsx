10305e2480ca063ed311a36bd14299076a380714
Suprio-Das <priyamd507@gmail.com>
2025-02-16 01:08:55 +0600

    Navbar imported to the login page

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 
 const AuthLayout = () => {
     return (
-        <div>
-            AuthLayout
+        <div className="bg-[#F3F3F3]">
+            <header className="w-11/12 mx-auto">
+                <Navbar></Navbar>
+            </header>
             <Outlet></Outlet>
         </div>
     );
