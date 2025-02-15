9b870220bbcd9552a3c0d58110ca1eb4144de90b
Suprio-Das <priyamd507@gmail.com>
2025-02-16 01:32:09 +0600

    Login page more styled

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <h2 className="text-center pt-5 text-2xl font-semibold">Login your account</h2>
                 <div className="card-body">
                     <form className="fieldset">
-                        <label className="fieldset-label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
-                        <label className="fieldset-label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <label className="fieldset-label text-black font-semibold">Email Address</label>
+                        <input type="email" className="input outline-0 border-0 bg-[#F3F3F3]" placeholder="Email" />
+                        <label className="fieldset-label text-black font-semibold">Password</label>
+                        <input type="password" className="input outline-0 border-0 bg-[#F3F3F3]" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
-                        <button className="btn btn-neutral mt-4">Login</button>
+                        <button className="btn bg-[#403F3F] text-white mt-4">Login</button>
                     </form>
+                    <h2 className="text-center text-[#706F6F]">Don't have an account? <Link to='/auth/register' className="text-red-500 font-semibold">Register</Link></h2>
                 </div>
             </div>
         </div>
