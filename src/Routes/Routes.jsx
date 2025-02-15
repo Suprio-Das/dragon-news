aee0295412c51006f1777459660781257f56e433
Suprio-Das <priyamd507@gmail.com>
2025-02-16 01:22:46 +0600

    Login Page designed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomeLayout from "../Layouts/HomeLayout";
 import CategoryNews from "../Pages/CategoryNews";
 import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Login";
 
 const Routes = createBrowserRouter([
     {
@@ -29,7 +30,7 @@ const Routes = createBrowserRouter([
         children: [
             {
                 path: '/auth/login',
-                element: <h1>Login</h1>
+                element: <Login></Login>
             },
             {
                 path: '/auth/register',
