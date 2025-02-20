b674c4be4283ac445a18a23fa854c26bef35f03c
Suprio-Das <priyamd507@gmail.com>
2025-02-20 21:37:18 +0600

    NewsDetails body layout created

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -8,6 +8,14 @@ const NewsDetails = () => {
                 <Header></Header>
                 <Navbar></Navbar>
             </header>
+            <main className="grid grid-cols-12 justify-center">
+                <div className="col-span-9">
+                    <h1>Single News Details</h1>
+                </div>
+                <div className="col-span-3">
+                    <h1>Right Sidebar</h1>
+                </div>
+            </main>
         </div>
     );
 };
