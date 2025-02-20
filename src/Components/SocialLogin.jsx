7504df4139649497a731dcef3bca6a47c29719a8
Suprio-Das <priyamd507@gmail.com>
2025-02-20 21:01:56 +0600

    Resolved - signUpWithGit function added to the context function

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -3,11 +3,20 @@ import { IoLogoGithub } from "react-icons/io";
 import { AuthContext } from "../Provider/AuthProvider";
 import { useContext } from "react";
 const SocialLogin = () => {
-    const { signUpWithGoogle } = useContext(AuthContext);
+    const { signUpWithGoogle, signUpWithGit } = useContext(AuthContext);
     const handleGoogleLogin = () => {
         signUpWithGoogle()
             .then(result => {
                 console.log(`Google Signin complete, ${result.user}`);
+            })
+            .catch(error =>
+                console.log(error.message)
+            )
+    }
+    const handleGitLogin = () => {
+        signUpWithGit()
+            .then(result => {
+                console.log(`Git Signin complete, ${result.user}`);
             })
             .catch(error =>
                 console.log(error.message)
@@ -18,7 +27,7 @@ const SocialLogin = () => {
             <h2 className="font-semibold text-[20px] my-1">Login With</h2>
             <div>
                 <button onClick={handleGoogleLogin} className="btn my-1 w-full text-blue-500"><FaGoogle className="text-blue-500" /> Login With Google</button>
-                <button className="btn my-1 w-full"><IoLogoGithub className="text-lg" /> Login With Github</button>
+                <button onClick={handleGitLogin} className="btn my-1 w-full"><IoLogoGithub className="text-lg" /> Login With Github</button>
             </div>
         </div>
     );
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -42,6 +42,7 @@ const AuthProvider = ({ children }) => {
 
     const authInfo = {
         signUpWithGoogle,
+        signUpWithGit,
         createNewUser,
         signInUser,
         logout,
