f4af76894ab9a7fd68d040d5b2f4f2b39c7fb314
Suprio-Das <priyamd507@gmail.com>
2025-02-20 20:28:26 +0600

    Google signup added to the AuthContext

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase/Firebase.config";
 
@@ -6,16 +6,27 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+
+    // Creating New User with Google
+    const googleProvider = new GoogleAuthProvider();
+
+    const signUpWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
     // Creating New User
     const createNewUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
     const logout = () => {
         return signOut(auth);
     }
+
     const signInUser = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
+
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
@@ -24,13 +35,16 @@ const AuthProvider = ({ children }) => {
             unSubscribe();
         }
     }, [])
+
     const authInfo = {
+        signUpWithGoogle,
         createNewUser,
         signInUser,
         logout,
         setUser,
         user
     }
+
     console.log(user);
     return (
         <AuthContext.Provider value={authInfo}>
