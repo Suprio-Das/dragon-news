305b5f2f2b3fe05be326d181157b3f973f03d632
Suprio-Das <priyamd507@gmail.com>
2025-02-21 01:28:16 +0600

    Button changed to Link and linked to current category

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 import { FaArrowLeftLong } from "react-icons/fa6";
@@ -20,11 +20,11 @@ const NewsDetails = () => {
                         <h1 className="font-semibold text-xl text-[#403F3F]">Dragon News</h1>
                         <div className="shadow-sm p-5 mt-3 rounded-md">
                             <div>
-                                <img src={singleNewsData.image_url} alt="" />
+                                <img src={singleNewsData.image_url} alt="" className="w-full" />
                             </div>
                             <h1 className="text-[25px] text-[#403F3F] font-semibold my-2">{singleNewsData.title}</h1>
                             <p className="text-base text-[#403F3F] leading-6 text-justify">{singleNewsData.details}</p>
-                            <button className="btn bg-[#D72050] text-white mt-7"><FaArrowLeftLong /> All news in this category</button>
+                            <Link to={`/category/${singleNewsData.category_id}`} className="btn bg-[#D72050] text-white mt-7"><FaArrowLeftLong /> All news in this category</Link>
                         </div>
                     </div>
                 </div>
